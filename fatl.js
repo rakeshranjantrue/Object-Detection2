d16418d646ed8cc9e065666ab93eeb68ee3c21d6
rakeshranjantrue <86142402+rakeshranjantrue@users.noreply.github.com>
2023-03-20 20:23:35 +0530

    Add files via upload

---
+++ fatl.js
@@ -0,0 +1,30 @@
+Status = "";
+fatl_image = "";
+
+function preload(){
+    fatl_image = loadImage("fatl.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(640, 350);
+    canvas.position(315, 200);
+    object_Detector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    object_Detector.detect(fatl_image, gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+}
+
+function draw(){
+    image(fatl_image, 0, 0, 640, 350);
+}
